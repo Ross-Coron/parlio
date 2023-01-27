a18009310e7b8637b27373b227b2be94d1614c57
Ross P. Coron <rpcoron@gmail.com>
2023-01-27 23:36:29 +0000

    Began annunciator page structure

--- parlio/urls.py
+++ parlio/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("annunciator", views.annunciator, name="annunciator"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
